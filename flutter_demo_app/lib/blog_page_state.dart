8160fd7229c959c90d76d612925be6684ecaa122
RonanLabs <ronanlabs0521@gmail.com>
2022-08-01 23:20:51 +0800

    add blog page

---
+++ flutter_demo_app/lib/blog_page_state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_app/blog_page.dart';
+
+class BlogPageState extends State<BlogPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
